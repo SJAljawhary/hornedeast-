64a8855a79c91529fde7272d813756aa4e3d00f7
SJAljawhary <sndjehad@gmail.com>
2021-06-27 21:14:48 +0300

    finishing

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+  render() {
+    return (
+
+        <h1 className='headerName'>HornedBoast</h1>
+    )
+  }
+}
+export default Header;
+
