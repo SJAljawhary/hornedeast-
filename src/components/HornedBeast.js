7ce3770e9377bf6e44cc28b827243f571a70fa69
SJAljawhary <sndjehad@gmail.com>
2021-06-28 17:26:33 +0300

    finishing lab02

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,19 +1,54 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 
 
 class HornedBeast extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        numberOfLikes: 0,
+
+    }
+
+}
+increaseLikes = () => {
+    this.setState({
+      numberOfLikes: this.state.numberOfLikes + 1
+    })
+}
+
     render() {
       return (
   
-         <div className='animalData'> 
+        
+            <Card className='cardStyle'>
+                  
+                  <Card.Body className='cardBody'>
+                      <Card.Title className='cardTitle'>{this.props.title}</Card.Title>
+                      <Card.Img  variant="top" src={this.props.url} alt={this.props.title} />
+                      <Card.Text className='cardDesc'>
+                          {this.props.description}
+                      </Card.Text>
+                      <Card.Text className='cardKey'>
+                          {this.props.keyword}
+                      </Card.Text>
+                      <Card.Text className='horns'>
+                          {this.props.horns}
+                      </Card.Text>
+                      <Card.Text className='vote'>
+                      Vote for your favourite One !!
+                      </Card.Text>
+                      <Button className='buttonStyle' variant="warning" onClick={this.increaseLikes} >VOTE</Button>{' '}
+                      <Card.Text className='likes'>
+                      Likes Number : {this.state.numberOfLikes}
+                      </Card.Text> 
+                  </Card.Body>
+              </Card>
 
-               <h2 className='animalName'> {this.props.title} </h2>
-
-               <img className='images' src={this.props.imageURL} alt={this.props.imageURL} ></img>
-               
-               <p className='description'>{this.props.description}</p>
-
-             </div>
 
       )
     }
