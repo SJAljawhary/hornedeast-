0a2f754089635d16e90e8be4aaa84fe276348b13
SJAljawhary <sndjehad@gmail.com>
2021-06-28 15:15:10 +0300

    editing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import HornedBeast from './components/HornedBeast';
+
 
 import './App.css';
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ class Main extends React.Component {
     render() {
       return (
   
-         <div>
+         <div>    
 
 <HornedBeast
    title = "UniWhal"
